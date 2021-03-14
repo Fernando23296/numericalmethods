7376293553f81bd47c2e562a113403baab8043f0
Fernando Silva <fernandosilva@MacBook-Pro-de-Fernando.local>
2021-03-14 17:53:59 -0400

    matrix for 2x2

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    global columna
+    global fila
     if request.method=='POST':
         columna = int(request.form['columna'])
         fila = int(request.form['fila'])
@@ -14,12 +16,13 @@ def index():
 @app.route('/matrix', methods=['GET','POST'])
 def matrix():
     if request.method=='POST':
+
         f0c0 = int(request.form['fila0columna0'])
-        f0c1 = int(request.form['fila0columna0'])
-        f1c0 = int(request.form['fila0columna0'])
-        f1c1 = int(request.form['fila0columna0'])
+        f0c1 = int(request.form['fila0columna1'])
+        f1c0 = int(request.form['fila1columna0'])
+        f1c1 = int(request.form['fila1columna1'])
         suma = f0c0+f0c1+f1c0+f1c1
-        return render_template('resultado.html', suma=suma)
+    return render_template('suma.html', suma = suma, columna2=columna2, fila2=fila2  )
 if __name__ == "__main__":
 
     app.run(debug=True)
