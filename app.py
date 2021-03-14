c59e83127f1d3e73ea8e41c9ebac66e9a2ca09fe
fernando <fernando23296@gmail.com>
2021-03-14 16:46:15 -0400

    web

--- app.py
+++ app.py
@@ -8,10 +8,18 @@ def index():
         columna = int(request.form['columna'])
         fila = int(request.form['fila'])
         identidad = np.eye(columna,fila)
-        return render_template('velocidad.html', columna=columna, fila=fila, identidad = identidad)
+        return render_template('matrix.html', columna=columna, fila=fila, identidad = identidad)
     return render_template('index.html')
 
-
+@app.route('/matrix', methods=['GET','POST'])
+def matrix():
+    if request.method=='POST':
+        f0c0 = int(request.form['fila0columna0'])
+        f0c1 = int(request.form['fila0columna0'])
+        f1c0 = int(request.form['fila0columna0'])
+        f1c1 = int(request.form['fila0columna0'])
+        suma = f0c0+f0c1+f1c0+f1c1
+        return render_template('resultado.html', suma=suma)
 if __name__ == "__main__":
 
     app.run(debug=True)
